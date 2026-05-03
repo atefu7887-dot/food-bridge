c69dcdf01b0f7ba6d55907fc00915389e0e6c990
atef <atefu7877@gmail.com>
2026-05-03 18:38:08 +0200

    upate Done

--- models/User.js
+++ models/User.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
     email: {
         type: String,
         required: true,
         unique: true,
         lowercase: true,
+    },
+    phone: {
+        type: String,
+        required: true,
     },
     password: {
         type: String,
