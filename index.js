37aec9cbb3bcde36c55c5e4ff366836bef079614
atef <atefu7877@gmail.com>
2026-05-06 03:06:56 +0200

    final update

--- index.js
+++ index.js
@@ -13,23 +13,12 @@ app.use(cors());
 
 
 const authRouter = require('./routes/auth');
-const donationsRouter = require('./routes/donations');
 const forgotPasswordRouter = require('./routes/forgot_password');
-const messagesRouter = require('./routes/messages');
-const notificationsRouter = require('./routes/notifications');
-const receiversRouter = require('./routes/receiver');
-const volunteerRouter = require('./routes/volunteer');
-const donorRouter = require('./routes/donations'); 
 
 
 app.use('/auth', authRouter);
-app.use('/api/donations', donationsRouter);
+
 app.use('/auth/forgot-password', forgotPasswordRouter);
-app.use('/api/messages', messagesRouter);
-app.use('/api/notifications', notificationsRouter);
-app.use('/api/receiver', receiversRouter);
-app.use('/api/volunteer', volunteerRouter);
-app.use('/api/donor', donorRouter);
 
 
 mongoose.connect(process.env.MONGO_URI)
