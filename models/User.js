f14f570cce133e74e4ac69cba5c85b10c685e00f
atef <atefu7877@gmail.com>
2026-05-11 23:28:38 +0200

    final update

--- models/User.js
+++ models/User.js
@@ -1,84 +1,90 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
-{
-    username: {
-        type: String,
-        required: true,
-        trim: true
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+
+        phone: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        password: {
+            type: String,
+            required: true,
+            select: false
+        },
+
+        role: {
+            type: String,
+            enum: ['Donor', 'Receiver', 'Driver'],
+            required: true
+        },
+
+        donorType: {
+            type: String,
+            default: ""
+        },
+
+        receiverType: {
+            type: String,
+            default: ""
+        },
+
+        businessName: {
+            type: String,
+            default: ""
+        },
+
+        address: {
+            type: String,
+            default: ""
+        },
+
+        avatar: {
+            type: String,
+            default: ""
+        },
+
+        licenseImage: {
+            type: String,
+            default: ""
+        },
+
+        resetPasswordOtp: {
+            type: String
+        },
+        resetPasswordExpire: {
+            type: Date
+        },
+
+        isVerified: {
+            type: Boolean,
+            default: false
+        },
+        availability: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Availability',
+            default: null
+        }
+
     },
-
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
-    },
-
-    phone: {
-        type: String,
-        required: true,
-        trim: true
-    },
-
-    password: {
-        type: String,
-        required: true,
-        select: false
-    },
-
-    role: {
-        type: String,
-        enum: ['Donor', 'Receiver', 'Driver'],
-        required: true
-    },
-
-    donorType: {
-        type: String,
-        default: ""
-    },
-
-    receiverType: {
-        type: String,
-        default: ""
-    },
-
-    businessName: {
-        type: String,
-        default: ""
-    },
-
-    address: {
-        type: String,
-        default: ""
-    },
-
-    avatar: {
-        type: String,
-        default: ""
-    },
-
-    licenseImage: {
-        type: String,
-        default: ""
-    },
-
-    isVerified: {
-        type: Boolean,
-        default: false
-    },
-
-    availability: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Availability',
-        default: null
-    }
-
-},
-{
-    timestamps: true
-});
+    {
+        timestamps: true
+    });
 
 //////////////////////////////////////////////////
 // PRE SAVE MIDDLEWARE
